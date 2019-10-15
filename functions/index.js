1527880d12be7289d03e3fdd2fe1429b123917cb
Murilo Moraes <kliffkast@gmail.com>
2019-10-15 17:37:29 -0300

    Initial tests

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,16 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/home', (request, response) =>
 {
- response.send('fuck yeah');
+ fs.readFile('../public/index.html',(err,data) =>
+ {
+  if (err) throw err;
+  response.send(data,'html');
+ });
 });
 
 exports.app = functions.https.onRequest(app);
