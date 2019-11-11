781c1dadd8c823ee34f5ae4b079352553a5cc38e
Murilo Moraes <kliff@vast-horizon.singlehorizon.com>
2019-11-11 18:17:06 -0300

    A lot of shit done

---
+++ functions/src/js/scripts.js
@@ -0,0 +1,17 @@
+module.exports = { script: '<script type="text/javascript">' +
+    '            $(document).ready(function () {' +
+    '                $("#sidebar").mCustomScrollbar({' +
+    '                    theme: "minimal"' +
+    '                });' +
+    '                $(\'#dismiss, .overlay\').on(\'click\', function () {' +
+    '                    $(\'#sidebar\').removeClass(\'active\');' +
+    '                    $(\'.overlay\').fadeOut();' +
+    '                });' +
+    '                $(\'#sidebarCollapse\').on(\'click\', function () {' +
+    '                    $(\'#sidebar\').addClass(\'active\');' +
+    '                    $(\'.overlay\').fadeIn();' +
+    '                    $(\'.collapse.in\').toggleClass(\'in\');' +
+    '                    $(\'a[aria-expanded=true]\').attr(\'aria-expanded\', \'false\');' +
+    '                });' +
+    '            });' +
+    '        </script>' };
