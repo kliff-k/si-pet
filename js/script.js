1ac09a657370700e6a917d93b6c85974d2641b4b
kliff-k <0yaghamy0>
2019-11-12 17:03:27 -0300

    PWA Tests

--- js/script.js
+++ js/script.js
@@ -12,15 +12,3 @@ $(()=>{
         $('#sidenav-overlay').addClass('active');
     });
 });
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../sw.js', {
-        scope: '/si-pet/' // <--- THIS BIT IS REQUIRED
-    }).then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
-}
