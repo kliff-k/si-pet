7f8d0dd21b070795a304dd9eaf50bab4c65e11bb
kliff-k <0yaghamy0>
2019-11-12 17:09:13 -0300

    PWA Tests

--- app/index.php
+++ app/index.php
@@ -1,14 +1,5 @@
 <?php
 
-header("Service-Worker-Allowed: /");
-
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-}
-
 $page = $_GET['page']?$_GET['page']:'home';
 
 $body   = file_get_contents("./pages/$page.php");
