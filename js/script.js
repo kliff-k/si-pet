2ac0cf6e601cbde82cd27e9c6a811c2787552e5c
kliff-k <0yaghamy0>
2019-11-12 16:34:46 -0300

    PWA Tests

--- js/script.js
+++ js/script.js
@@ -14,11 +14,13 @@ $(()=>{
 });
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../js/sw.js')
-        .then(function () {
-            console.log('service worker registered');
-        })
-        .catch(function () {
-            console.warn('service worker failed');
-        });
+    navigator.serviceWorker.register('../js/sw.js', {
+        scope: '.' // <--- THIS BIT IS REQUIRED
+    }).then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
 }
