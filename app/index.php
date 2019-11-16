7f4a1c21ef79111bb1db8faa41b244cc7d4d383c
kliff-k <0yaghamy0>
2019-11-16 19:14:28 -0300

    Teste CURL

--- app/index.php
+++ app/index.php
@@ -1,14 +1,10 @@
 <?php
 
-session_start();
 
-if(!$_SESSION['si-pet-id'])
-    $page = 'login';
-else
     $page = $_GET['page']?$_GET['page']:'home';
 
-$body   = execCurl("https://localhost/si-pet/pages/$page.php");
-$main   = execCurl("https://localhost/si-pet/pages/main.php");
+$body   = file_get_contents("../pages/$page.php");
+$main   = file_get_contents("../pages/main.php");
 $main   = str_replace("{active-$page}", 'active', $main);
 $main   = str_replace("{body}", $body, $main);
 
@@ -19,6 +15,8 @@ function execCurl ($url)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // Temp! For localhost testing only. TODO: Remove
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Temp! For localhost testing only. TODO: Remove
     $output = curl_exec($ch);
     curl_close($ch);
 
