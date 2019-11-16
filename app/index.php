c370cc6b1c5d7ac1b1be06782abb2868c7a14376
kliff-k <0yaghamy0>
2019-11-16 14:30:22 -0300

    More tweaks

--- app/index.php
+++ app/index.php
@@ -2,8 +2,8 @@
 
 $page = $_GET['page']?$_GET['page']:'home';
 
-$body   = execCurl("http://localhost/si-pet/pages/$page.php");
-$main   = execCurl("http://localhost/si-pet/pages/main.php");
+$body   = execCurl("https://localhost/si-pet/pages/$page.php");
+$main   = execCurl("https://localhost/si-pet/pages/main.php");
 $main   = str_replace("{active-$page}", 'active', $main);
 $main   = str_replace("{body}", $body, $main);
 
@@ -14,6 +14,8 @@ function execCurl ($url)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // Temp! For localhost testing only.
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Temp! For localhost testing only.
     $output = curl_exec($ch);
     curl_close($ch);
 
