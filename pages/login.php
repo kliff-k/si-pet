9356f0aaaa9be33d46919a09993b3f6ecc1b2206
kliff-k <0yaghamy0>
2019-11-16 16:34:51 -0300

    Backend pretty much done

---
+++ pages/login.php
@@ -0,0 +1,23 @@
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+                <form action="" method="">
+                    <div class="form-group row">
+                        <label for="email_address" class="col-md-4 col-form-label text-md-right">SI Pet ID</label>
+                        <div class="col-md-6">
+                            <input type="text" id="si-pet-id" class="form-control" name="si-pet-id" required autofocus>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 offset-md-4">
+                        <button type="button" class="btn btn-primary" id="activate-id">
+                            Ativar
+                        </button>
+                    </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
