6de3fa6ee54f14dd6c3b3dc1947af3e053c99d46
kliff-k <0yaghamy0>
2019-11-16 19:17:30 -0300

    Teste CURL

--- app/index.php
+++ app/index.php
@@ -7,20 +7,9 @@ if(!$_SESSION['si-pet-id'])
 else
     $page = $_GET['page']?$_GET['page']:'home';
 
-$body   = execCurl("https://localhost/si-pet/pages/$page.php");
-$main   = execCurl("https://localhost/si-pet/pages/main.php");
+$body   = file_get_contents("../pages/$page.php");
+$main   = file_get_contents("../pages/main.php");
 $main   = str_replace("{active-$page}", 'active', $main);
 $main   = str_replace("{body}", $body, $main);
 
 echo $main;
-
-function execCurl ($url)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-
-    return $output;
-}
